b21dc7d089d9d5503b0c80a152469a41e21dd541
thaissa <thaissarib04@gmail.com>
2026-04-10 10:00:14 -0300

    Início da criação das classes Produto e Pedido

---
+++ src/test/java/br/dev/thaissa/FastFuriousFood/FastFuriousFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.thaissa.FastFuriousFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFuriousFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
